9e67f715235bd31a6dc251050682366779ca6194
Alberto Luebbert <alberto@ideashappy.com>
2022-11-30 00:31:05 -0600

    :rocket: Implementación Release Ci/CD App

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  },
 }
 
 module.exports = nextConfig
