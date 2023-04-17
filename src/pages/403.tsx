662bdee0763ee92505db5829863362fc142593ad
Alberto Luebbert <alberto@ideashappy.com>
2023-04-17 12:43:33 -0600

    :sparkles: Error 403 y Error 404 agregados

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -3,15 +3,15 @@ import React, { useState } from 'react';
 import { Layout } from '@/components/layouts';
 import { ErrorContainer } from '@/containers/ErrorC';
 
-const Error404: NextPage = () => {
+const Error403: NextPage = () => {
   const [showIconMenu] = useState<boolean>(true);
   const [title] = useState<string>('Aviso de Privacidad');
   const [changeIconMenu, setChangeIconMenu] = useState<boolean>(false);
   return (
     <Layout headTitle={title} showIconMenu={showIconMenu} changeIconMenu={changeIconMenu} setChangeIconMenu={setChangeIconMenu}>
-      <ErrorContainer messageError="404" />
+      <ErrorContainer messageError="403" />
     </Layout>
   );
 };
 
-export default Error404;
+export default Error403;
